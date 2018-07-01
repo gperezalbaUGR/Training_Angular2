32c075ad83c440b4e0405eae0f04a6e8b0dbe665
gperezalba <gperezalba94@gmail.com>
2018-07-01 19:49:19 +0200

    Font-awesome y Card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatInputModule, MatSortModule, MatTableModule, MatDialogModule } from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatCardModule} from '@angular/material/card';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -10,6 +12,7 @@ import { FilmListComponent } from './film-list/film-list.component';
 import { AddDialogComponent } from './add-dialog/add-dialog.component';
 import { FormComponent } from './form/form.component';
 import { TablaComponent } from './tabla/tabla.component';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
 
 @NgModule({
   declarations: [
@@ -28,7 +31,10 @@ import { TablaComponent } from './tabla/tabla.component';
     MatTableModule,
     HttpModule,
     FormsModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    Angular2FontawesomeModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
